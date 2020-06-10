dd154eb50274037886e3539e9928f6b0be0d50f7
BrenoLuna0 <breno.luna_@hotmail.com>
2020-06-10 12:15:15 -0300

    mudança na status bar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,80 +20,82 @@ function Login({ navigation }) {
   const [modalVisibility, setModalVisibility] = useState(false);
 
   return (
-    <KeyboardAvoidingView
-      //resetScrollToCoords={{ x: 0, y: 0 }}
-      //contentContainerStyle={styles.containerMaster}
-      //scrollEnabled={false}
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      style={styles.containerMaster}
-    >
+    <>
       <StatusBar backgroundColor="#3B3F8C" barStyle="light-content" />
-      <LoadingScreen visible={modalVisibility} transparent={true} />
-      <View style={styles.header}>
-        <View style={styles.containerChild}>
-          <Image
-            style={styles.containerChildImage}
-            source={require("../../assets/club_logo.png")}
+      <KeyboardAvoidingView
+        //resetScrollToCoords={{ x: 0, y: 0 }}
+        //contentContainerStyle={styles.containerMaster}
+        //scrollEnabled={false}
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={styles.containerMaster}
+      >
+        <LoadingScreen visible={modalVisibility} transparent={true} />
+        <View style={styles.header}>
+          <View style={styles.containerChild}>
+            <Image
+              style={styles.containerChildImage}
+              source={require("../../assets/club_logo.png")}
+            />
+          </View>
+        </View>
+        <View style={{ marginTop: 100 }}></View>
+        <View style={styles.inputBlock}>
+          <TextInput
+            keyboardType={"number-pad"}
+            type={"custom"}
+            options={{
+              mask,
+            }}
+            value={numTitulo}
+            onChangeText={(text) => {
+              if (text.length > 5) {
+                setMask("999.999.999-99");
+              } else {
+                setMask("999999999");
+              }
+              setNumTitulo(text);
+            }}
+            mask
+            label="Número do Titular ou CPF"
           />
         </View>
-      </View>
-      <View style={{ marginTop: 100 }}></View>
-      <View style={styles.inputBlock}>
-        <TextInput
-          keyboardType={"number-pad"}
-          type={"custom"}
-          options={{
-            mask,
-          }}
-          value={numTitulo}
-          onChangeText={(text) => {
-            if (text.length > 5) {
-              setMask("999.999.999-99");
+        <View style={styles.inputBlock}>
+          <TextInput
+            value={senha}
+            secureTextEntry={true}
+            onChangeText={(senha) => setSenha(senha)}
+            label="Senha"
+          />
+        </View>
+        <View style={{ height: 5 }}></View>
+        <DefaultButton
+          onPress={async () => {
+            setModalVisibility(true);
+            const result = await onSignIn(numTitulo, senha);
+            if (result) {
+              setNumTitulo("");
+              setSenha("");
+              navigation.navigate("Home", {});
+              setModalVisibility(false);
             } else {
-              setMask("999999999");
+              setModalVisibility(false);
+              alert("Numero de titular ou senha inválida");
             }
-            setNumTitulo(text);
           }}
-          mask
-          label="Número do Titular ou CPF"
+          title={"ENTRAR"}
         />
-      </View>
-      <View style={styles.inputBlock}>
-        <TextInput
-          value={senha}
-          secureTextEntry={true}
-          onChangeText={(senha) => setSenha(senha)}
-          label="Senha"
-        />
-      </View>
-      <View style={{ height: 5 }}></View>
-      <DefaultButton
-        onPress={async () => {
-          setModalVisibility(true);
-          const result = await onSignIn(numTitulo, senha);
-          if (result) {
-            setNumTitulo("");
-            setSenha("");
-            navigation.navigate("Home", {});
-            setModalVisibility(false);
-          } else {
-            setModalVisibility(false);
-            alert("Numero de titular ou senha inválida");
-          }
-        }}
-        title={"ENTRAR"}
-      />
-      <Text style={styles.link}>Esqueceu a senha?</Text>
+        <Text style={styles.link}>Esqueceu a senha?</Text>
 
-      <View style={styles.register}></View>
-      <DefaultButton
-        onPress={() => {
-          navigation.navigate("Register", {});
-        }}
-        title={"CADASTRAR"}
-      />
-      <View style={{ height: 60 }}></View>
-    </KeyboardAvoidingView>
+        <View style={styles.register}></View>
+        <DefaultButton
+          onPress={() => {
+            navigation.navigate("Register", {});
+          }}
+          title={"CADASTRAR"}
+        />
+        <View style={{ height: 60 }}></View>
+      </KeyboardAvoidingView>
+    </>
   );
 }
 
